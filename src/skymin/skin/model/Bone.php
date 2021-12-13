25950cae172754391b2d8a744811281a4f6973e2
skymin <ppt00003333@gmail.com>
2021-12-13 15:56:22 +0900

    .

--- src/skymin/skin/model/Bone.php
+++ src/skymin/skin/model/Bone.php
@@ -3,9 +3,11 @@ declare(strict_types = 1);
 
 namespace skymin\skin\model;
 
+use function array_merge;
+
 final class Bone{
 	
-	public function __construct(public ModelManager $manager, private array $bone){}
+	public function __construct(public ModelManager $manager, public array $bone){}
 	
 	public function getName() : string{
 		return $this->bone['name'];
@@ -27,6 +29,19 @@ final class Bone{
 	
 	public function getPivot() : array{
 		return $this->bone['pivot'];
+	}
+	
+	public function getCubes() : ?array{
+		if(isset($this->bone['cubes'])){
+			return $this->bone['cubes'];
+		}
+		return null;
+	}
+	
+	public function mergeCubes(array $cubes) : void{
+		if(isset($this->bone['cubes'])){
+			$this->bone['cubes'] = array_merge($this->bone['cubes'], $cubes)
+		}
 	}
 	
 }
--- src/skymin/skin/model/ModelManager.php
+++ src/skymin/skin/model/ModelManager.php
@@ -6,13 +6,23 @@ namespace skymin\skin\model;
 final class ModelManager{
 	
 	private array $bones;
+	private string $name;
+	private float $width;
+	private float $height;
+	private array $offset;
 	
-	public function __construct(private array $model){
+	public function __constuct(private array $model){
 		$this->init();
 	}
 	
 	public function init(){
-		foreach($this->model['minecraft:geometry'][0]['bones'] as $key => $value){
+		$model = $this->model['minecraft:geometry'][0];
+		$des = $model['description'];
+		$this->name = $des['identfifier'];
+		$this->width = $des['visible_bounds_width'];
+		$this->height = $des['visible_bounds_height'];
+		$this->offset = $des['visible_bounds_offset'];
+		foreach($model['bones'] as $key => $value){
 			$bone = new Bone($value, $this);
 			$this->bones[$bone->getName()] = $bone;
 		}
@@ -27,6 +37,22 @@ final class ModelManager{
 	
 	public function getBones() : array{
 		return $this->bones;
+	}
+	
+	public function getName() : string{
+		return $this->name;
+	}
+	
+	public function getWidth() : float{
+		return $this->width;
+	}
+	
+	public function getHeight() : float{
+		return $this->float;
+	}
+	
+	public function getOffset() : array{
+		return $this->offset;
 	}
 	
 }
